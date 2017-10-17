da784eba59f2e9c01fec21a330c43193d0c8cd96
Mohit Shridhar <mohit.appi@gmail.com>
2017-10-17 10:32:55 +0800

    updating README

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # DESPOT Tutorials
 
-An example of using [DESPOT](https://github.com/AdaCompNUS/despot) with real-robots via ROS. We implement a slightly modified version of the Laser Tag problem: a robot chases another robot with noisy laser-sensor observations. The simulation consists of two holonomic robots ([KUKA Youbot](http://www.youbot-store.com/)) inside a [Gazebo](http://gazebosim.org/) environment resembling the problem world described in [1] (Page 20).
+An example of using [DESPOT](https://github.com/AdaCompNUS/despot) with real-robots via ROS. We present a slightly modified version of the Laser Tag problem: a robot chases a target by using a noisy laser-sensor to observe its environment. When the robot is adjacent to the target, it calls 'Tag' to terminate a successful pursuit. The simulation consists of two holonomic robots ([KUKA Youbot](http://www.youbot-store.com/)) inside a [Gazebo](http://gazebosim.org/) environment resembling the problem world described in DESPOT paper (Page 20) [1].
 
 
 [1] N. Ye, A. Somani, D. Hsu, and W. Lee. [**DESPOT: Online POMDP planning with regularization**](http://bigbird.comp.nus.edu.sg/m2ap/wordpress/wp-content/uploads/2017/08/jair14.pdf). J. Artificial Intelligence Research, 58:231–266, 2017.
@@ -38,7 +38,7 @@ Install BOOST libraries with `sudo apt-get install libboost-all-dev`
 
 If you haven't sourced your ROS environment, run:
 ```bash
-$ source /opt/ros/indigo/setup.bash
+$ source /opt/ros/indigo/setup.bash OR <existing_workspace>/devel/setup.bash
 ```
 
 Setup a fresh catkin workspace for **despot_tutorials**:
@@ -46,7 +46,7 @@ Setup a fresh catkin workspace for **despot_tutorials**:
 ```bash
 $ mkdir -p ~/despot_ws/src
 $ cd ~/despot_ws/
-$ catkin_make
+$ catkin_make 
 $ source devel/setup.bash
 ```
 
@@ -59,7 +59,7 @@ $ git clone https://github.com/AdaCompNUS/despot_tutorials.git
 Compile:
 ```bash
 cd ~/despot_ws
-catkin_make
+catkin_make -DCMAKE_BUILD_TYPE=Release
 ```
 
 ## Usage
@@ -75,3 +75,7 @@ $ rosrun laser_tag pomdp_planner
 ```
 
 You should see a 3D 7x11 grid world with two Youbots. The green robot should chase the red robot until 'Tag' is called. The `R1_noise` parameter specifies the gaussian noise (standard deviation in meters) of the green robot's laser range finder.  
+
+## Guidelines
+
+In general, to use DESPOT with real-world systems: setup a simple interface to send actions and receive observations in the format specified in your POMDP model (e.g: `ACT_TYPE` & `OBS_TYPE` parameters). See [main.cpp](examples/laser_tag/src/pomdp_planner/main.cpp) for more details.
--- examples/laser_tag/src/pomdp_planner/main.cpp
+++ examples/laser_tag/src/pomdp_planner/main.cpp
@@ -10,6 +10,8 @@ using namespace despot;
 
 #define TAG 4
 #define DEFAULT_NOISE_SIGMA 0.5
+#define TERMINATION_OBSERVATION 101
+#define NUM_LASER_DIRECTIONS 8
 
 double noise_sigma_ = DEFAULT_NOISE_SIGMA;
 
@@ -19,17 +21,16 @@ public:
 
     ros::NodeHandlePtr nh;
     ros::ServiceClient client;
-    //Establish connection with simulator or system
+
     bool Connect(){
-        // initilize ROS node. Should be done just once
+        
+        // initialize ROS node
         int argc;
         char ** argv;
         ros::init(argc, argv, "test_laser_tag");
         nh = ros::NodeHandlePtr(new ros::NodeHandle);
-        // wait for controller service to show up (waits forever)
-        ros::service::waitForService("laser_tag_action_obs", -1);
         
-        // initalize laser
+        // get laser's noise sigma
         if (nh->getParam("/robot/youbot_interface/noise_sigma", noise_sigma_))
         {
           ROS_INFO("Initialized laser with noise: %f stddev.", noise_sigma_);
@@ -38,38 +39,54 @@ public:
         {
           ROS_INFO("Initialized laser with default noise: %f stddev.", noise_sigma_);
         }
+
+        // wait for laser tag controller service to show up (blocking call)
+        ros::service::waitForService("laser_tag_action_obs", -1);
+        
         // setup service client
         client = nh->serviceClient<laser_tag::TagActionObs>("laser_tag_action_obs");
     }
+
     //Initialize or reset the environment (for simulators or POMDP world only), return the start state of the system if applicable
     State* Initialize(){
         return NULL;
     }
+
     //Get the state of the system (only applicable for simulators or POMDP world)
     State* GetCurrentState(){
         return NULL;
     }
+
     //Send action to be executed by the system, receive observations terminal signals from the system
     bool ExecuteAction(ACT_TYPE action, OBS_TYPE& obs){
-        // send action & receive observation
+
+        /* laser_tag::TagActionObs is a ROS service that takes in an action (integer) 
+         * and outputs observations (8 intergers) after executing the action. If the 
+         * 'Tag' action is called, it returns a boolean 'tag_success' with the outcome.
+         */
+
         laser_tag::TagActionObs srv;
         srv.request.action = (int) action; // actions: 0 - North, 1 - East, 2 - South, 3 - West, 4 - Tag
         if (client.call(srv))
         {
+
+          // successful tag
           if(action == TAG && srv.response.tag_success == true)
           {
 	           obs=(OBS_TYPE)0;
-	           for (int dir = 0; dir < 8; dir++) {
-	               LaserTag::SetReading(obs, 101, dir);
+	           for (int dir = 0; dir < NUM_LASER_DIRECTIONS; dir++) {
+	               LaserTag::SetReading(obs, TERMINATION_OBSERVATION, dir);
 	           }
-            return 1;
+            return 1; // exit
 	        }
+
+          // continue to observe
           else 
           {
             // observations after executing actions
             std::vector<int> laser_obs = srv.response.observations;
 
-            // print observations (metric readings rounded to the nearest integer)
+            // print observations 
             ROS_INFO("Laser Observations");
             ROS_INFO("North: %d"    , laser_obs[0]);
             ROS_INFO("East: %d"     , laser_obs[1]);
@@ -83,13 +100,13 @@ public:
             for (int dir = 0; dir < 8; dir++) {
                LaserTag::SetReading(obs, laser_obs[dir], dir);
             }
-            return 0;
+            return 0; // continue
           }
         }
         else
         {
           ROS_ERROR("Failed to execute action & receive observations. Something went wrong with the robot controller!");
-          return 0;
+          return 0; // continue
         }
     }
 };
